8928e6c4e85d05730597e5576bdde55ead285612
Nicolai Stoianov <stoianovnk@gmail.com>
2021-04-28 18:42:33 +0300

    🇷🇺 Support Russian timestamps

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function validateType(fileList) {
 function readFile(file) {
   const reader = new FileReader();
   const separator = '==========';
-  const timestampRegex = /Added on */;
+  const timestampRegex = /Added on|Добавлено\:*/;
 
   reader.onload = function(event) {
     var contents = event.target.result;
